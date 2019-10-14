ccfdaa82718cf27975d8000957ac2d67444802f5
Gui Vieira <guialbuk@gmail.com>
2019-10-13 23:13:59 -0700

    Handle incoming connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use log::{error, info};
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    simple_logger::init().unwrap();
+    info!("Starting server...");
+
+    let ip = "127.0.0.1:8080";
+
+    let listener = TcpListener::bind(ip).expect("Unable to create listener");
+
+    info!("Server started on {} {}", "http://", ip);
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_) => info!("Request received!"),
+            Err(error) => error!("Connection failed: {}", error),
+        }
+    }
 }
